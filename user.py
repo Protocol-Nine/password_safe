535b89b6413f65c94f786b45f129ce06a39ca704
Erik Slader <eslader@uri.edu>
2024-04-20 21:22:30 -0400

    Added encryption and decryption for user password pairs.

--- user.py
+++ user.py
@@ -1,4 +1,8 @@
 import json
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Util.Padding import pad, unpad
+from base64 import b64encode, b64decode
 
 class User:
     def __init__(self, username, password, pairs=None):
@@ -7,10 +11,23 @@ class User:
         self.pairs = pairs if pairs is not None else []
         
     def add_pair(self, website, password):
-        self.pairs.append({"website": website, "password": password})
+        key = self.derive_key()
+        cipher = AES.new(key, AES.MODE_CBC, b'0000000000000000')
+        encrypted_password = cipher.encrypt(pad(password.encode('utf-8'), AES.block_size))
+        self.pairs.append({"website": website, "password": b64encode(encrypted_password).decode('utf-8')})
         
     def list_pairs(self):
-        return self.pairs
+        decrypted_pairs = []
+        key = self.derive_key()
+        for pair in self.pairs:
+            cipher = AES.new(key, AES.MODE_CBC, b'0000000000000000')
+            encrypted_password = b64decode(pair['password'])
+            decrypted_password = unpad(cipher.decrypt(encrypted_password), AES.block_size).decode('utf-8')
+            decrypted_pairs.append({"website": pair['website'], "password": decrypted_password})
+        return decrypted_pairs
+    
+    def derive_key(self):
+        return PBKDF2(self.password,b'salt', 32, 100000)
         
     def to_dict(self):
         return{
